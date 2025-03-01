95467f7982fd5e4475f4311f7fe4a2bbfcd118a8
aparnaroy <aparnar@udel.edu>
2025-03-01 15:39:41 -0500

    added basic profile page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import logo from './assets/full-logo.png'
 import './App.css'
 import ChatBot from './Chatbot'
+import ProfilePage from './Profile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
           <img src={logo} className="logo" alt="Vite logo" />
         </a>
       </div>
+      <ProfilePage />
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
