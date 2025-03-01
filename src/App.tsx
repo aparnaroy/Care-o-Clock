a8ffca3bb8275aa0414e33ebce9fa3fb734f840e
Aman Singh <amansjot@gmail.com>
2025-03-01 14:52:42 -0500

    updated logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from './assets/full-logo.png'
 import './App.css'
 import ChatBot from './Home'
 
@@ -11,10 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={logo} className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
