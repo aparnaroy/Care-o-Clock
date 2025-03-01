9f824bb63706f2412f43aca0d72cfe0bad635d0e
benita <beniabra@udel.edu>
2025-03-01 18:59:54 -0500

    fixed login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
-import { useState } from 'react'
-import logo from './assets/full-logo.png'
-import './App.css'
-import ChatBot from './Chatbot'
-import UserProfile from './components/UserProfile'
-import { Login } from './Login'
+import logo from "./assets/full-logo.png";
+import "./App.css";
+import ChatBot from "./Chatbot";
+import UserProfile from "./components/UserProfile";
+import { Login } from "./Login";
 // import ProfilePage from './Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={logo} className="logo" alt="Vite logo" />
-        </a>
+        <img src={logo} className="logo" alt="Vite logo" />
       </div>
       {/* <ProfilePage /> */}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <Login />
-        <Login />
-        <ChatBot />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login />
+      <ChatBot />
       <UserProfile />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
